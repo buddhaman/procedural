9ae29de009bdd9ea740d6e81f6fbef79ccc8edb2
Buddhaman <tim.trussner@gmail.com>
2025-08-16 21:27:47 +0200

    Now nice build static

--- procedural-terrain/vite.config.ts
+++ procedural-terrain/vite.config.ts
@@ -3,4 +3,24 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/demos/procedural/',
+  worker: {
+    format: 'es',
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[hash][extname]',
+        chunkFileNames: '[name].[hash].js',
+        entryFileNames: '[name].[hash].js'
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  }
 });
